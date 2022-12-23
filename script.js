a6176150ebf5da9e0ccc27cf4aedf23164bac008
MindulaHewadewage <mindufernando@gmail.com>
2022-12-23 20:01:22 +0100

    dati

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const dati = [
+    {
+        profilePic:'https://unsplash.it/300/300?image=15',
+        author:'Phil Mangione',
+        time:'22/12/2022',
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint',
+        postImage:'https://unsplash.it/600/300?image=171'
+    },
+
+    {
+        profilePic:'https://unsplash.it/300/300?image=15',
+        author:'Mario Rossi',
+        time:'20/10/2007',
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint',
+        postImage:'https://unsplash.it/600/300?image=171'
+    },
+
+    {
+        profilePic:'https://unsplash.it/300/300?image=15',
+        author:'Lorenzo Verdi',
+        time:'15/07/2021',
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint',
+        postImage:'https://unsplash.it/600/300?image=171'
+    },
+
+    {
+        profilePic:'https://unsplash.it/300/300?image=15',
+        author:'Giacomo Bianchi',
+        time:'05/10/2020',
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint',
+        postImage:'https://unsplash.it/600/300?image=171'
+    },
+
+    {
+        profilePic:'https://unsplash.it/300/300?image=15',
+        author:'Marco Neri',
+        time:'13/05/2021',
+        text:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint',
+        postImage:'https://unsplash.it/600/300?image=171'
+    },
+
+    
+]
+
+
